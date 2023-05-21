abeeac19a7195e4fd75fd5660f907ed4583a8870
David Wurtz <davidjwurtz@gmail.com>
2023-05-20 17:41:28 -0700

    hook up reviewer

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter, HTTPException, Request, Response
+from reviewer import review_pr
 
 router = APIRouter()
 
@@ -7,7 +8,20 @@ async def handle_github_webhooks(request: Request, response: Response):
     print("Received a github webhook")
     body = await request.json()
     print(body)
+    action = body.get("action", None)
+    # number = body.get("number", None)
+    pull_request = body.get("pull_request", {})
+    if action == "opened":
+        url = pull_request.get("url", None)
+        if url is None:
+            return {}
+        try:
+            review_pr(url)
+        except Exception as e:
+            print(e)
     return {}
+        
+        
 
 app = FastAPI()
 app.include_router(router, prefix="/api/v1")
